06cd5312c86bc8d5a3b8faab42ad82dfa0e60548
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-02-06 15:57:12 -0500

    added columns to models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {through: ProductTag})
 
 module.exports = {
   Product,
